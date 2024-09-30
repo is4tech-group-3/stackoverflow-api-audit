590a08ec924ec4e4dfdc72e6b774e386da6254f4
wcabrera-2022060 <wcabrera-2022060@kinal.edu.gt>
2024-09-30 00:30:00 -0600

    pagination in data obtained from audit

--- src/audit.controller.js
+++ src/audit.controller.js
@@ -14,8 +14,50 @@ export const createAudit = async (req, res) => {
 
 export const getAudits = async (req, res) => {
   try {
-    const audits = await Audit.find()
-    return res.status(200).json(audits)
+    const { startDate, endDate, limit = 10, page = 1 } = req.query
+    const limitNumber = parseInt(limit)
+    const pageNumber = parseInt(page)
+    const skips = limitNumber * (pageNumber - 1)
+
+    let query = {}
+
+    if (startDate && endDate) {
+      query = {
+        date_operation: {
+          $gte: new Date(startDate),
+          $lt: new Date(endDate)
+        }
+      }
+    }
+
+    if (startDate) {
+      query = {
+        date_operation: {
+          $gte: new Date(startDate)
+        }
+      }
+    }
+
+    if (endDate) {
+      query = {
+        date_operation: {
+          $lt: new Date(endDate)
+        }
+      }
+    }
+
+    const audits = await Audit.find(query).skip(skips).limit(limitNumber)
+    const totalAudits = await Audit.countDocuments(query)
+    const totalPages = Math.ceil(totalAudits / limitNumber)
+
+    return res.status(200).json({
+      audits,
+      pagination: {
+        totalAudits,
+        totalPages,
+        currentPage: pageNumber
+      }
+    })
   } catch (error) {
     return res.status(500).json({ message: error.message })
   }
